b0051f788ebd799be0d783afe82dfa3e38cb008b
M4xwo <woelfle.max@googlemail.com>
2024-04-25 14:58:45 +0200

    finished script and added explanation

--- exec_alg.sh
+++ exec_alg.sh
@@ -1,8 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 
-time1=$(date +%s)
-sleep 10 
-time2=$(date +%s)
-time_elapsed=$(($time2 - $time1))
+/LETHE/COURSES/HPC24SS/AliScale/query ##query directory 
+/LETHE/COURSES/HPC24SS/AliScale/references ##reference directory
+time1=$(date +%s)                          ##takes the date before running the command to calculate the time afterwards
+
+vsearch --usearch_global /LETHE/COURSES/HPC24SS/AliScale/query/$2 --db /LETHE/COURSES/HPC24SS/AliScale/references/$3 -t $1
+##vsearch searches for correct alignments betweend two fasta files. Directory of the file has to be given, file name is taken form loop script 
+##
+
+time2=$(date +%s)                           ##second date is taken for time calculation 
+time_elapsed=$(($time2 - $time1))           ##calculation of time and storage in variable
+echo "$time_elapsed seconds; $1 threads used; number of sequences: $2; size of database: $3 "  >> testfile.txt ##one fancy output just for the sake of it 
+
+
+echo "$time_elapsed seconds; $1;  $2; $3"  >> results_max.txt  ##one output to further work with 
+
+
+
+
 
--- loop_skript.sh
+++ loop_skript.sh
@@ -1,25 +1,25 @@
 #!/bin/sh 
 
-threads=(1)
-seqlength=(3 1)
-Nseq=(1 16)
-db_size=(1)
+##script for a nested loops that iterate over every combination from used threats, query-file, reference-file and give every iteration 
+##to the second script which gives the combinations into a vsearch 
 
-for db in ${db_size[@]}
+
+threads=(1 5 10) ##number of used threats 
+Nseq=$(ls /LETHE/COURSES/HPC24SS/AliScale/query) ##directory of query files 
+db_size=$(ls /LETHE/COURSES/HPC24SS/AliScale/references) ##directory of reference files
+rm testfile.txt ##deletes any files from previous runs 
+rm results_max.txt ##deletes any files from previous runs 
+for db in $db_size;
     do    
-        for num in ${Nseq[@]}
-            do    
-                for length in ${seqlength[@]}
-                    do    
-                        for amount in ${threads[@]}    
-                            do     
-                            
-                            srun -c 10 sh exec_alg.sh $Threads $seqlength $Nseq $db_size
-                            ##ficke
+        for num in $Nseq;
+            do      
+                for amount in ${threads[@]};    
+                    do     
+                    sleep 1                                                 ##sleep to slow down the flood of jobs 
+                    srun -c 10 sh exec_alg.sh $threads $Nseq $db_size &     ##srun manages jobs and after the script name used values of the current 
+                                                                            ##iteration are given to the vseasrch script         
                              
-                        done   
+                           
                 done
         done
 done
-
-
