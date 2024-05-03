07612022ea1d23d2b7be35c2445500a63d34993c
M4xwo <woelfle.max@googlemail.com>
2024-05-03 15:01:28 +0200

    bacgen plots and final script changes

--- BacGen/bac_gen_script.sh
+++ BacGen/bac_gen_script.sh
@@ -11,9 +11,12 @@ total_count=$(($ac_content+$gc_content))
 gc_ratio=$(echo "scale=5; $gc_content/$total_count" | bc)
 
 
-num_genes=$(grep -o -i "gene" $workdir/$index/*.gff | wc -m) 
-
-echo "$index; $total_count; $num_genes; $gc_ratio " >> bac_results_xah.txt
+num_genes=$(grep -o -i "gene" $workdir/$index/*.gff | wc -m)
+##getting bac ID step 2 friday
+bac_id=$(grep -i "##species" $workdir/$index/*.gff | head -n 1 | cut -d"=" -f2)
+phylum=$(docker run --rm ncbi/edirect  /bin/bash -c  "efetch -db taxonomy -id $bac_id -format xml | xtract -pattern Taxon -block '*/Taxon' -tab '\n'  -element Id,Rank,TaxId,ScientificName" | grep "phylum" | cut -f 3)
+##
+echo "$index; $total_count; $num_genes; $gc_ratio; $phylum " >> bac_results_xah2.txt
 
 
 
